abc064dbad834a084599a446e746de96fc53b81e
Atchokogue <77995797+Atchokogue@users.noreply.github.com>
2021-02-23 01:04:43 -0500

    late night study

--- exercice.py
+++ exercice.py
@@ -8,16 +8,34 @@ import itertools
 
 
 def get_maximums(numbers):
-	return [0]
+	liste_finale = []
+	for listes in numbers:
+		liste_ordre = sorted(listes, reverse=True)
+		liste_finale += liste_ordre[0:1]
+
+	return [liste_finale]
+
 
 def join_integers(numbers):
-	return 0
+	string_nbr = ""
+	for elem in numbers:
+		string_nbr += str(elem)
+	return int(string_nbr)
+
 
 def generate_prime_numbers(limit):
-	return [0]
+	premiers = []
+	nombres = list(range(2, limit + 1))
+	while len(nombres) != 0:
+		premiers.append(nombres[0])
+		nombres = [elem for elem in nombres if elem % nombres[0] != 0]
+
+	return premiers
+
 
 def combine_strings_and_numbers(strings, num_combinations, excluded_multiples):
 	return [""]
+
 
 if __name__ == "__main__":
 	print(get_maximums([[1,2,3], [6,5,4], [10,11,12], [8,9,7]]))
@@ -25,7 +43,7 @@ if __name__ == "__main__":
 	print(join_integers([111, 222, 333]))
 	print(join_integers([69, 420]))
 	print("")
-	print(generate_prime_numbers(17))
+	print(generate_prime_numbers(77))
 	print("")
 	print(combine_strings_and_numbers(["A", "B"], 2, None))
 	print(combine_strings_and_numbers(["A", "B"], 5, 2))
